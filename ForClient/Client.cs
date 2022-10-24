031f6bb198d327edb3a459231b8226c90e8685fc
Anastasiia <monanastya.2004@gmail.com>
2022-10-25 01:02:29 +0300

    Initial version

---
+++ ForClient/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Google.Protobuf;
+using ProtoBuf;
+using System.Net.Sockets;
+using System.Text;
+
+namespace new_tcp
+{
+     class Client
+    {
+        private const int port = 8080;
+        private const string ip_server = "127.0.0.1"; // might be output variable
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(ip_server, port);
+                SendingMessage sm;
+                NetworkStream str = client.GetStream();
+               
+                sm = Serializer.Deserialize<SendingMessage>(str);
+                Console.WriteLine(" WHO: {sm.UserName}\n WHEN: {sm.SendingTime}\n MESSAGE: {sm.Mess}");
+                Console.ReadLine();
+                str.Close(); client.Close();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Socket exceprion: " + e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Other exceprion: " + e);
+            }
+        }
+    }
+}
---
+++ ForLintener/Listener.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Google.Protobuf;
+using ProtoBuf;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace new_tcp
+{
+     class Listener
+    {
+        private const int port = 8080;
+        private const string ip_server = "127.0.0.1";
+
+        static void Main(string[] args)
+        {
+            SendingMessage mess;
+            TcpListener server; // for working withiut blocks 
+            try
+            {
+                IPAddress local_ip = IPAddress.Parse(ip_server);
+                server = new TcpListener(local_ip, port);
+                server.Start();
+               while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Client accept sucsessfuly");
+                    NetworkStream str = client.GetStream();
+                    Console.WriteLine("What are you need?");
+                    mess = new SendingMessage {Mess = Console.ReadLine(), UserName = ip_server, SendingTime = DateTime.Now.ToString() };
+                    str.Write(mess.ToByteArray(), 0, mess.CalculateSize());
+                    str.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Socket exceprion: " + e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Other exceprion: " + e);
+            }
+        }
+    }
+}
